0b89a37703d7b82c74e4f33484d17f5f5b4cefb6
Jack Sellars <jsellars@F3C025.42adel.org.au>
2022-02-23 16:03:06 +1030

    added error log message and reformatted all logs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jsellars <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 17:07:42 by jsellars          #+#    #+#             */
-/*   Updated: 2022/02/23 15:27:01 by jsellars         ###   ########.fr       */
+/*   Updated: 2022/02/23 16:02:33 by jsellars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 
 int	main(void)
 {
-	char	str[20];
+	char	str[22];
 	char	*ret;
 
-	printf("**initialized string variables:\n");
-	printf("**str at %p\n**ret at %p\n", &str, &ret);
+	printf(">>initialized string variables:\n");
+	printf(">>str at %p\n>>ret at %p\n", &str, &ret);
 	printf("Enter your name: ");
 	scanf("%21s", str);
-	while (str[19] != '\0')
+	while (ft_strlen(str) > 20)
 	{
+		printf(">>User input exceeds character limit (20)\n");
 		printf("Name field cannot be more than x characters,");
 		printf("try again.\nEnter your name: ");
-		ft_bzero(str, 4);
 		scanf("%21s", str);
 	}
-	printf("**value of str after user input: %s\n", str);
+	printf(">>value of str after user input: %s\n", str);
 	ret = ft_strjoin("Welcome to FDS - ", str);
-	printf("**value of ret after ft_strjoin: %s\n", ret);
+	printf(">>value of ret after ft_strjoin: %s\n", ret);
 	printf("%s\n", ret);
 	return (0);
 }
